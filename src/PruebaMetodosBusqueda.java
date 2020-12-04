1b37cec166c6d09eefb66229a128b6538654dbe3
RicardoReyes44 <rickyboy1002@gmail.com>
2020-12-04 14:25:43 -0600

    Se agrego la clase 'Metodosbusqueda' y se le añadio el metodo 'binarySearch'

--- src/PruebaMetodosBusqueda.java
+++ src/PruebaMetodosBusqueda.java
@@ -1,8 +1,31 @@
 
+class MetodosBusqueda{
+	
+	public boolean binarySearch(int[] data, int target, int low, int high) {
+		if (low > high)
+			return false;
+		else {
+			int mid = (low + high) / 2;
+			if (target == data[mid])
+				return true;
+			else if (target < data[mid])
+				return binarySearch(data, target, low, mid - 1);
+			else
+				return binarySearch(data, target, mid + 1, high);
+		}
+	}
+	
+}
+
+
 public class PruebaMetodosBusqueda {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		MetodosBusqueda mb = new MetodosBusqueda();
+		
+		int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		System.out.println(mb.binarySearch(array, 12, 0, array.length-1));
 
 	}
 
